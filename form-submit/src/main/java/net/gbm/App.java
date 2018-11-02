d0cf400f10631bd15437a7f82fd4ecacc4abc182
Anubis Lockward <anubis.lockward@gmail.com>
2018-11-02 12:01:59 -0400

    First commit to Form Submit project

---
+++ form-submit/src/main/java/net/gbm/App.java
@@ -0,0 +1,13 @@
+package net.gbm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
