90e9409e078a15880c3e56311bf8301a7ccc172c
Anubis <anubis.lockward@gmail.com>
2018-11-03 15:05:26 -0400

    Finished implementing the View and Model parts of the App.
    The view is handled by the greeting.html and result.html.
    The model is in the Greeting class.
    Made the program executable as a Spring Boot Application.

--- form-submit/src/main/java/net/gbm/model/Greeting.java
+++ form-submit/src/main/java/net/gbm/model/Greeting.java
@@ -1,5 +1,28 @@
 package net.gbm.model;
 
 public class Greeting {
+	private Long id;
+	private String content;
+
+	public String getContent() {
+		return content;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	@Override
+	public String toString() {
+		return "Greeting [id=" + id + ", content=" + content + "]";
+	}
 
 }
